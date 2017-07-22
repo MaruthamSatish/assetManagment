85958690c2969a40ddd2d01e11226ad8a148675e
AMMANANASATISH <AMMANANASATISH@192.168.2.11>
2017-07-22 10:51:55 +0530

    TTTTTTT

--- src/main/java/com/javaBuddies/assetManagment/HomeController.java
+++ src/main/java/com/javaBuddies/assetManagment/HomeController.java
@@ -31,6 +31,7 @@ public class HomeController {
 		
 		String formattedDate = dateFormat.format(date);
 		System.out.println("DemoTTTTT");
+		System.out.println("Demo");
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
